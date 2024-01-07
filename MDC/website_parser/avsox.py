0805672fe1e06ba0074fc661b3acc7cbfc7755ea
tanpengsccd <tanpengcd@gmail.com>
2024-01-08 07:56:56 +0800

    feat: 解析器修改

--- MDC/website_parser/avsox.py
+++ MDC/website_parser/avsox.py
@@ -1,7 +1,40 @@
 # 使用avsox 站点
+from lxml import html
+import requests
 from MDC.website_parser.website_parser import WebsiteParser
 
 
 class Avsox(WebsiteParser):
 
-    
+    def _get_available_site_base_url(self, number: str) -> str:
+        """ 获取可用的站点地址 """
+        # 访问 https://tellme.pw/avsox 获取最新网址
+        # 获取HTML内容
+        url = "https://tellme.pw/avsox"
+        response = requests.get(url)
+        tree = html.fromstring(response.content)
+        # 使用XPath获取数据
+        links = tree.xpath('//div[@class="container"]/div/a/@href')
+        return links[0] if links else ''
+
+    base_url = _get_available_site_base_url()  # 'https://avsox.click'
+
+    def search_number(self, number: str) -> [str]:
+        """ 搜索番号,返回网页链接列表 """
+        url = self.base_url + '/cn/search/' + number
+        response = requests.get(url)
+        tree = html.fromstring(response.content)
+        # 使用XPath获取数据
+        links = tree.xpath('//*[@id="waterfall"]/div/a/@href')
+        return [self.base_url + link for link in links]
+
+    @property
+    @abstractmethod
+    def detail_html(self) -> str:
+        """ 网页内容 """
+        raise NotImplementedError
+
+    @abstractmethod
+    def Parser(self) -> NfoModel.Movie:
+        """ 解析网页,返回NfoModel.Movie"""
+        raise NotImplementedError
--- MDC/website_parser/website_parser.py
+++ MDC/website_parser/website_parser.py
@@ -6,7 +6,6 @@ from typing import Type, TypeVar, Generic
 from MDC.nfo_model import NfoModel
 
 
-
 class WebsiteParser(ABC):
     """ 本意是使用抽象类来强制实现类必须实现对应 nfo属性key 的 xpath,但是 vscode 现在还无法强制提示未实现的属性或方法, 只有pycharm 可以强制提示
     """
@@ -17,22 +16,18 @@ class WebsiteParser(ABC):
         """ 站点baseUrl,有的站点可能会经常变化,需根据实际情况获取或修改 """
         raise NotImplementedError
 
-    @property
     @abstractmethod
-    def search_path(self) -> str:
-        """ 搜索path 和base_url组合成完整的搜索地址 """
+    def search_number(self, number: str) -> [str]:
+        """ 搜索番号,返回详情页面地址 """
         raise NotImplementedError
-
 
     @property
     @abstractmethod
     def detail_html(self) -> str:
         """ 网页内容 """
         raise NotImplementedError
-    
+
     @abstractmethod
     def Parser(self) -> NfoModel.Movie:
         """ 解析网页,返回NfoModel.Movie"""
         raise NotImplementedError
-
-
